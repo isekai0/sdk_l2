2c01d8c79c5d8c8cebbec0a7fbd39792d7832713
Doug Molina <dmolina79@users.noreply.github.com>
2020-08-18 11:08:06 -0600

    add missing type exports (#11)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { StablePayLayer2Manager as Layer2ProviderManager } from './StablePayLayer2Manager';
 export { StablePayLayer2Provider as Layer2Provider } from './StablePayLayer2Provider';
+export { Layer2WalletBuilder } from './Layer2WalletBuilder';
+export { Layer2Wallet } from './Layer2Wallet';
 export * from './types';
 export * from './Operation';
