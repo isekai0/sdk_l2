5cd6c6f38fe8ee89bc70164f0d61193b900db470
Gabriel Molina <xgmolinax@gmail.com>
2021-02-13 16:39:38 -0600

    fix: expose AccountStream from lib (#33)

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { Layer2Manager as Layer2ProviderManager } from './Layer2Manager';
 export { Layer2Provider } from './Layer2Provider';
 export { Layer2WalletBuilder } from './Layer2WalletBuilder';
 export { Layer2Wallet } from './Layer2Wallet';
+export { default as AccountStream } from './AccountStream';
 export * from './types';
 export * from './Operation';
