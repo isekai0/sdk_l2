cf4a25238c07c8fa1281f6e131f7992a7189a878
Rodimiro Cerrato E <rodyce@gmail.com>
2021-05-17 00:18:49 -0600

    Separate LooprinResult tests

--- test/loopring.spec.ts
+++ test/loopring.spec.ts
@@ -3,8 +3,7 @@ import { Layer2Manager } from '../src/Layer2Manager';
 import { Layer2Provider } from '../src/Layer2Provider';
 import { Layer2WalletBuilder } from '../src/Layer2WalletBuilder';
 import { Layer2Wallet } from '../src/Layer2Wallet';
-import { Deposit, Withdrawal, Transfer } from '../src/Operation';
-import { LoopringResult } from '../src/loopring/LoopringResult';
+import { Deposit } from '../src/Operation';
 
 import { ethers } from 'ethers';
 
@@ -88,66 +87,6 @@ describe('Integration tests (require connection to a real service)', () => {
     expect(receipt.operationType).toBe(OperationType.Deposit);
     expect(receipt.to).toBe(myAddress);
     expect(receipt.committed).toBeTruthy();
-  });
-
-  it('Verify correct fee calculation on deposits', async () => {
-    // Test setup.
-    // I am going to deposit to my own address in L2.
-    const myAddress = layer2Wallet.getAddress();
-    // Data for fee calculation.
-    // Gas price of 50 GWei
-    const sampleGasPriceHex = '0xba43b7400';
-    const sampleGasUsed = 73_512;
-    const expectedFee = '0.0036756';
-
-    // Create Deposit data.
-    const deposit = Deposit.createDeposit({
-      toAddress: myAddress,
-      amount: '0.1', // Desired amount
-      fee: '0', // Desired fee. This is a LAYER ONE regular fee.
-    });
-
-    // Sample received transactions.
-    const tx: ethers.Transaction & Record<string, any> = {
-      chainId: 1,
-      nonce: 5,
-      data: 'dummy data',
-      gasLimit: ethers.BigNumber.from('300000'),
-      gasPrice: ethers.BigNumber.from(sampleGasPriceHex),
-      value: ethers.BigNumber.from('0x470de4df820000'),
-    };
-
-    // Sample transaction receipt.
-    const txReceipt: ethers.ContractReceipt = {
-      to: myAddress,
-      from: myAddress,
-      contractAddress: '0x2e76EBd1c7c0C8e7c2B875b6d505a260C525d25e',
-      transactionIndex: 0,
-      gasUsed: ethers.BigNumber.from(sampleGasUsed),
-      logsBloom: '',
-      blockHash: 'ha',
-      blockNumber: 4,
-      confirmations: 2,
-      transactionHash: '',
-      logs: [],
-      cumulativeGasUsed: ethers.BigNumber.from('0x0ba43b7400'),
-      byzantium: true,
-    };
-
-    // Mock wait() async function to return the transaction's receipt.
-    tx['wait'] = async (): Promise<ethers.ContractReceipt> => {
-      return txReceipt;
-    };
-
-    // Fake Loopring operation result.
-    const loopringResult = new LoopringResult(tx, deposit);
-
-    // Method under test.
-    const receipt = await loopringResult.getReceipt();
-
-    // Expectations.
-    // Expect correct fee calculation.
-    expect(receipt.fee).toBe(expectedFee);
   });
 });
 
